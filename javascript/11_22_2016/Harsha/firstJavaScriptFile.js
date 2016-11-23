e4b8a5e2e18da57de5849d8b7c3eae9836f4b871
harsha <harsha.wgd60@gmail.com>
2016-11-22 21:08:17 -0600

    JS Assignment

---
+++ javascript/11_22_2016/Harsha/firstJavaScriptFile.js
@@ -0,0 +1,39 @@
+//  number primitive data type
+var x = 8;
+var y = 12354657586969;
+var z = 3845.35473;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+//  string primitive data type
+var firstName = 'Harsha';
+var lastName = 'K';
+var address = 'MO, USA';
+
+console.log(firstName);
+console.log(lastName);
+console.log(address);
+
+
+//  boolean primitive data type
+var  isRegisteredUser= true;
+var isItLoggedIn = false;
+
+console.log(isRegisteredUser);
+console.log(isItLoggedIn);
+
+
+//  null primitive data type
+var a = null;
+
+console.log(a);
+
+
+//  undefined primitive data type
+var r = undefined;
+
+console.log(r);
+
