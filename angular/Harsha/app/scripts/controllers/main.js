ba8e3f62bbd988cd9eac2e8cdceccfb80d383d3e
harsha <harsha.wgd60@gmail.com>
2017-01-05 19:09:49 -0600

    Angular

---
+++ angular/Harsha/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name harshaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the harshaApp
+ */
+angular.module('harshaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
